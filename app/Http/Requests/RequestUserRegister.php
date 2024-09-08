f3782172640b622f615be1937a2ca80df848bf47
tientran <kimtientran0410@gmail.com>
2024-09-08 17:21:19 +0700

    update file RequestUserRegister

--- app/Http/Requests/RequestUserRegister.php
+++ app/Http/Requests/RequestUserRegister.php
@@ -3,11 +3,13 @@
 namespace App\Http\Requests;
 
 
+use App\Traits\APIResponse;
 use Illuminate\Contracts\Validation\Validator;
 use Illuminate\Foundation\Http\FormRequest;
 use Illuminate\Http\Exceptions\HttpResponseException;
 class RequestUserRegister extends FormRequest
 {
+    use APIResponse;
     /**
      * Determine if the user is authorized to make this request.
      *
@@ -34,13 +36,8 @@ class RequestUserRegister extends FormRequest
     public function failedValidation(Validator $validator)
     {
         $errors = $validator->errors()->all();
-        throw new HttpResponseException(response()->json([
-            'success' => false,
-            'message' => 'Validation errors',
-            'data' => $errors,
-            'errors' => $validator->errors(),
-            'status' => 422,
-        ], 422));
+
+        return $this->responseErrorValidate($errors, $validator);
     }
 
     public function messages()
